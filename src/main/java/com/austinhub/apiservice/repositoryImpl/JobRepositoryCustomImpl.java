38b965a846fdd93708703a69cae16f46ba162dc8
ssdd2013 <shutao2011@gmail.com>
2021-06-27 23:11:59 -0500

    SQ-13: update repositories to handle all and other categories

--- src/main/java/com/austinhub/apiservice/repositoryImpl/JobRepositoryCustomImpl.java
+++ src/main/java/com/austinhub/apiservice/repositoryImpl/JobRepositoryCustomImpl.java
@@ -11,17 +11,22 @@ import java.math.BigInteger;
 import java.util.List;
 
 public class JobRepositoryCustomImpl implements JobRepositoryCustom {
+    private static final Integer ALL_CATEGORY_ID = 12;
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
     public int countAll(final int categoryId, final String queryStr) {
+        final String categoryIdStr = ALL_CATEGORY_ID.equals(categoryId)
+                ? ""
+                : "categoryId=" + categoryId + " and";
         final Query query = entityManager.createNativeQuery(
-                String.format("select count(1) from job where categoryId=%1$s"
-                        + " and ( name like '%2$s'"
+                String.format("select count(1) from job"
+                        + " where %1$s"
+                        + " (name like '%2$s'"
                         + " or address like '%2$s'"
-                        + " or description like '%2$s')", categoryId, "%" + queryStr + "%")
+                        + " or description like '%2$s')", categoryIdStr, "%" + queryStr + "%")
         );
         return ((BigInteger) query.getSingleResult()).intValue();
     }
@@ -35,16 +40,19 @@ public class JobRepositoryCustomImpl implements JobRepositoryCustom {
             final OrderBy orderBy
     ) {
         final int offset = page * pageSize;
+        final String categoryIdStr = ALL_CATEGORY_ID.equals(categoryId)
+                ? ""
+                : "job.categoryId=" + categoryId + " and";
         final String orderByStr = getOrderByStr(orderBy);
         final Query query = entityManager.createNativeQuery(
                 String.format(
                         "select * from job"
                                 + " left join category on job.categoryId=category.id"
                                 + " left join resource on job.resourceId=resource.id"
-                                + " where job.categoryId=%1$s"
-                                + " and (job.name like '%2$s' or job.address like '%2$s' or job.description like '%2$s')"
+                                + " where %1$s"
+                                + " (job.name like '%2$s' or job.address like '%2$s' or job.description like '%2$s')"
                                 + " order by %3$s limit %4$s offset %5$s",
-                        categoryId, "%" + queryStr + "%", orderByStr, pageSize, offset),
+                        categoryIdStr, "%" + queryStr + "%", orderByStr, pageSize, offset),
                 Job.class
         );
         return query.getResultList();
